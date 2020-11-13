5b6abaa6f9c283c60aa27a12731f982383e7a8dc
Jorge <jirazabal@flatiron.com>
2020-11-12 21:47:32 -0500

    Upgrade frontend

--- scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh root@vps65488.inmotionhosting.com "sudo -H -u jirazabal bash -c 'bash /home/jirazabal/scripts/deploy_script.sh'"
-
-ssh root@vps65488.inmotionhosting.com "systemctl restart baby_tracker"
-
--- scripts/startup_script.sh
+++
@@ -1,3 +0,0 @@
-source /home/jirazabal/.my_bashrc
-cd /home/jirazabal/repos/baby_tracker_be
-source /home/jirazabal/miniconda3/bin/activate bt && python src/main.py
